6be81245c6191314cb5a0aa8e9df87f024c629a9
Muhammed Ahmed <muhammad.farghali14@gmail.com>
2020-04-24 15:19:24 +0200

    Finish Task Two

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -12,6 +12,11 @@ class Bot:
         print(f'Hello! My name is {self.name}.'
               f'\nI was created in {self.birth_year}.')
 
+    def ask_for_name(self):
+        name = input('Please, remind me your name.\n')
+        print(f'What a great name you have, {name}!')
+
 
 my_bot = Bot('FAR14')
 my_bot.i_am()
+my_bot.ask_for_name()
