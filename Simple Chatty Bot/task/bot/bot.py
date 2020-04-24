6839693652a85a7936223358d6e3ee1548cf1155
Muhammed Ahmed <muhammad.farghali14@gmail.com>
2020-04-24 15:01:32 +0200

    Finish Task One

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,1 +1,17 @@
 # write your code here
+from datetime import datetime
+
+
+class Bot:
+    birth_year = datetime.now().year
+
+    def __init__(self, name):
+        self.name = name
+
+    def i_am(self):
+        print(f'Hello! My name is {self.name}.'
+              f'\nI was created in {self.birth_year}.')
+
+
+my_bot = Bot('FAR14')
+my_bot.i_am()
