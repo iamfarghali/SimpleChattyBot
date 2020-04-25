2f9c742ba699375b362731893ae3eb75a4547ddd
Muhammed Ahmed <muhammad.farghali14@gmail.com>
2020-04-25 11:27:17 +0200

    Finish Task Three

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -16,7 +16,16 @@ class Bot:
         name = input('Please, remind me your name.\n')
         print(f'What a great name you have, {name}!')
 
+    def guess_age(self):
+        print('Let me guess your age.\nSay me remainders of dividing your age by 3, 5 and 7.')
+        remainder3 = int(input())
+        remainder5 = int(input())
+        remainder7 = int(input())
+        age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+        print(f"Your age is {age}; that's a good time to start programming!")
+
 
 my_bot = Bot('FAR14')
 my_bot.i_am()
 my_bot.ask_for_name()
+my_bot.guess_age()
